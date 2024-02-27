c39d50e115e1019026cb8f06f0addb15b6c0168b
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-02-27 04:31:06 -0500

    Add patch-ag from NetBSD pkgsrc.
    Make FreeBSD patch for ld.so semantic differences between linux and bsd
    apply for NetBSD too: this closes PR/21452 (problem with gnucash not
    finding its symbols in dynamically loaded modules) for -currentish
    installations (this needs RTLD_DEFAULT support in ld_elf.so).
    1.6 installations still suffer unless a -current ld_elf.so and
    /usr/include/dlfcn.h are installed, sorry.
    
    Make other FreeBSD-specific patch apply too since it looks sensible
    (adds error checking).

--- gmodule/gmodule-dl.c
+++ gmodule/gmodule-dl.c
@@ -100,6 +100,9 @@ _g_module_open (const gchar *file_name,
 static gpointer
 _g_module_self (void)
 {
+#if defined(__FreeBSD__) || (defined(__NetBSD__) && defined(RTLD_DEFAULT))
+  return RTLD_DEFAULT;
+#else
   gpointer handle;
   
   /* to query symbols from the program itself, special link options
@@ -111,6 +114,7 @@ _g_module_self (void)
     g_module_set_error (fetch_dlerror ());
   
   return handle;
+#endif
 }
 
 static void
